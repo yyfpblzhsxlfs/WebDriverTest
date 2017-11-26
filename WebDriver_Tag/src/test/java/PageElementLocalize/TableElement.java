fcc8b9fd81f916676a6f1e0e1475a781b35d8a80
yyfpblzhsxlfs <372597752@qq.com>
2017-11-26 12:01:11 +0800

    add testLink test

--- WebDriver_Tag/src/test/java/PageElementLocalize/TableElement.java
+++ WebDriver_Tag/src/test/java/PageElementLocalize/TableElement.java
@@ -16,69 +16,69 @@ import Tools.BeginAndCloseChrome;
  * 8.11章节实例
  */
 public class TableElement {
-	// Define brower driver
-	private WebDriver driver;
+    // Define brower driver
+    private WebDriver driver;
 
-	@Test(groups = "LocateTable")
-	public void LocateTable() throws Exception {
-		BeginAndCloseChrome ba = new Tools.BeginAndCloseChrome("file:///E:/ZIXUE/web/table.html");
-		driver = ba.setUp();
+    @Test(groups = "LocateTable")
+    public void LocateTable() throws Exception {
+        BeginAndCloseChrome ba = new Tools.BeginAndCloseChrome("file:///E:/ZIXUE/web/table.html");
+        driver = ba.setUp();
 
-		WebElement table = driver.findElement(By.id("table"));
-		List<WebElement> rows = table.findElements(By.tagName("tr"));
+        WebElement table = driver.findElement(By.id("table"));
+        List<WebElement> rows = table.findElements(By.tagName("tr"));
 
-		assertEquals(5, rows.size());
+        assertEquals(5, rows.size());
 
-		for (WebElement row : rows) {
-			List<WebElement> cols = row.findElements(By.tagName("td"));
-			for (WebElement col : cols) {
-				System.out.print(col.getText() + "\t");
-			}
-			System.out.println();
-		}
-		ba.close();
-	}
+        for (WebElement row : rows) {
+            List<WebElement> cols = row.findElements(By.tagName("td"));
+            for (WebElement col : cols) {
+                System.out.print(col.getText() + "\t");
+            }
+            System.out.println();
+        }
+        ba.close();
+    }
 
 	/*
-	 * 定位表格中的某个单元格
+     * 定位表格中的某个单元格
 	 */
 
-	@Test(groups = "LocateTableTr")
-	public void LocateTableTr() throws Exception {
-		BeginAndCloseChrome ba = new Tools.BeginAndCloseChrome("file:///E:/ZIXUE/web/table.html");
-		driver = ba.setUp();
-		/*
+    @Test(groups = "LocateTableTr")
+    public void LocateTableTr() throws Exception {
+        BeginAndCloseChrome ba = new Tools.BeginAndCloseChrome("file:///E:/ZIXUE/web/table.html");
+        driver = ba.setUp();
+        /*
 		 * 以下两种定位方法任选， 一种是一次直接定位到所需标签
 		 * 一种是两次定位，在第二次定位时，使用//表示不识别具体位置，使用./表示从当前位置继续定位
 		 */
-		WebElement table1 = driver.findElement(By.xpath("//*[@id='table']/tbody/tr[2]/td[2]"));
-		String str1 = table1.getText();
+        WebElement table1 = driver.findElement(By.xpath("//*[@id='table']/tbody/tr[2]/td[2]"));
+        String str1 = table1.getText();
 
-		WebElement table2 = driver.findElement(By.xpath("//*[@id='table']"));
-		String str2 = table2.findElement(By.xpath("./tbody/tr[2]/td[2]")).getText();
+        WebElement table2 = driver.findElement(By.xpath("//*[@id='table']"));
+        String str2 = table2.findElement(By.xpath("./tbody/tr[2]/td[2]")).getText();
 
-		assertEquals("1000元", str1);
-		assertEquals("1000元", str2);
+        assertEquals("1000元", str1);
+        assertEquals("1000元", str2);
 
-		System.out.println(str1);
-		System.out.println(str2);
-		ba.close();
-	}
+        System.out.println(str1);
+        System.out.println(str2);
+        ba.close();
+    }
 
-	/*
-	 * 定位表格中的某个单元格
-	 */
-	@Test(groups = "LocateTableCheckBox")
-	public void LocateTableCheckBox() throws Exception {
-		BeginAndCloseChrome ba = new Tools.BeginAndCloseChrome("file:///E:/ZIXUE/web/tableChcekBox.html");
-		driver = ba.setUp();
-		WebElement WebchcekBox = driver.findElement(By.xpath("//table[@id='table']/tbody/tr[3]/td[1]/input[2]"));
+    /*
+     * 定位表格中的某个单元格
+     */
+    @Test(groups = "LocateTableCheckBox")
+    public void LocateTableCheckBox() throws Exception {
+        BeginAndCloseChrome ba = new Tools.BeginAndCloseChrome("file:///E:/ZIXUE/web/tableChcekBox.html");
+        driver = ba.setUp();
+        WebElement WebchcekBox = driver.findElement(By.xpath("//table[@id='table']/tbody/tr[3]/td[1]/input[2]"));
 
-		if (!WebchcekBox.isSelected()) {
-			WebchcekBox.click();
-		}
-		assertTrue(WebchcekBox.isSelected());
+        if (!WebchcekBox.isSelected()) {
+            WebchcekBox.click();
+        }
+        assertTrue(WebchcekBox.isSelected());
 
-		ba.close();
-	}
+        ba.close();
+    }
 }
